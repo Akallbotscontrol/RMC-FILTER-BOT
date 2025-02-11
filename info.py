3f922f9413c1743cbe7a517074b5ffe55e2e6e40
Ak all bots Control <akallbotscontrol@gmail.com>
2025-02-11 21:59:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,7 +7,7 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", "22289037"))
+API_ID       = int(environ.get("API_ID", "29034951"))
 API_HASH     = environ.get("API_HASH", "cfc8448de6746a6ccf9dc1812cf1c8f7")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "")
 DATABASE_URI = environ.get("DATABASE_URI", "")
